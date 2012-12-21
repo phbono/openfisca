8c98dc9b7fd46d401b108425ffd9b23bafd3e960
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2012-12-21 09:59:20 +0100

    Huge reorganization going on

--- src/france/model/cotsoc.py
+++ src/france/model/cotsoc.py
@@ -24,7 +24,7 @@ This file is part of openFisca.
 from __future__ import division
 from src.france.model.data import CAT
 from numpy import maximum as max_, minimum as min_, logical_not as not_, zeros, ones
-from src.core.utils import  scaleBaremes, combineBaremes, BaremeDict
+from src.core.utils_old import  scaleBaremes, combineBaremes, BaremeDict
 
 
                 
