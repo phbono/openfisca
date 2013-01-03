cc8d2823b63be6cf0a59f564606219c7b663643d
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2013-01-03 15:25:46 +0100

    Augmenting doc. Starting documenting France.

--- src/france/model/isf.py
+++ src/france/model/isf.py
@@ -1,25 +1,12 @@
 # -*- coding:utf-8 -*-
-# Copyright © 2012 Sarah Dijols
+#
+# This file is part of OpenFisca.
+# OpenFisca is a socio-fiscal microsimulation software
+# Copyright © 2011 Sarah Dijols, Clément Schaff, Mahdi Ben Jelloul
+# Licensed under the terms of the GPL (version 3 or later) license
+# (see openfisca/__init__.py for details)
 
-"""
-openFisca, Logiciel libre de simulation du système socio-fiscal français
-Copyright © 2011 Clément Schaff, Mahdi Ben Jelloul
 
-This file is part of openFisca.
-
-    openFisca is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    openFisca is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with openFisca.  If not, see <http://www.gnu.org/licenses/>.
-"""
 from __future__ import division
 from numpy import ( maximum as max_, minimum as min_) 
 from src.france.model.data import QUIFOY
--- src/france/model/pfam.py
+++ src/france/model/pfam.py
@@ -1,25 +1,10 @@
 # -*- coding:utf-8 -*-
+#
+# This file is part of OpenFisca.
+# OpenFisca is a socio-fiscal microsimulation software
 # Copyright © 2011 Clément Schaff, Mahdi Ben Jelloul
-
-"""
-openFisca, Logiciel libre de simulation du système socio-fiscal français
-Copyright © 2011 Clément Schaff, Mahdi Ben Jelloul
-
-This file is part of openFisca.
-
-    openFisca is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    openFisca is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with openFisca.  If not, see <http://www.gnu.org/licenses/>.
-"""
+# Licensed under the terms of the GPL (version 3 or later) license
+# (see openfisca/__init__.py for details)
 
 from __future__ import division
 from numpy import (round, floor, zeros, maximum as max_, minimum as min_,
